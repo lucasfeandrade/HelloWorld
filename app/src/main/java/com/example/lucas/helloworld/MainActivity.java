cc292b4504d469d6b0bcb5859e023d00d6b080b9
Lucas Fernandes Andrade <lucasfeandrade@gmail.com>
2016-07-29 20:16:50 -0300

    First Commit

--- app/src/main/java/com/example/lucas/helloworld/MainActivity.java
+++ app/src/main/java/com/example/lucas/helloworld/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Third Commit Test
+    //Fourth Commit Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
